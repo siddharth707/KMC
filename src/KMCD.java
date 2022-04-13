51f3a66742e8559735b15e9cc1648650c95a78c8
Siddharth Thakur <insthak@KMC.Corp>
2022-04-13 16:18:01 +0530

    first

---
+++ src/KMCD.java
@@ -0,0 +1,35 @@
+
+
+        String dsk = new String("dinsid");
+        String s = "dinwesh";
+        System.out.println("Siddharth");
+        System.out.println("Singhsiddharth");
+        System.out.println("thakur");
+        System.out.println();
+        System.out.println(i * j);
+        System.out.println(i);
+        System.out.println(r1);
+        System.out.println(r2);
+        System.out.println(r3);
+        System.out.println(r4);
+        System.out.println(s);
+        i = 60;
+        int i = 50;
+        int j = 10;
+        int m = 5, n = 2;
+        int r1 = m + n;
+        int r2 = m - n;
+        int r3 = m * n;
+        int r4 = m / n;
+    public class KMCD
+    public static void main(String arg[]) {
+    public static void main(String arg[]) {
+    public static void main(String args[]) {
+    }
+    }
+class SSDDLL {
+class School {
+system.out.println(s);
+}
+}
+}
